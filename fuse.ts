b6b111d9515b8f56ddcb01ffaba0c53cd31bd8de
Nathan Graule <solarliner@gmail;com>
2019-05-21 17:32:34 +0200

    Use Fusebox's task runner

--- fuse.ts
+++ fuse.ts
@@ -1,11 +1,46 @@
-import { FuseBox, QuantumPlugin } from "fuse-box";
+import { FuseBox, QuantumPlugin } from "fuse-box"
+import { src, task, exec, context } from "fuse-box/sparky"
 
-const fuse = FuseBox.init({
-  homeDir: ".",
-  target: "server@es5",
-  output: "dist/$name.js",
-  plugins: [QuantumPlugin({ uglify: true, treeshake: true })]
-});
+class FuseboxConfig {
+  forceMode?: "dev" | "prod"
 
-fuse.bundle("bin").instructions("> main.ts");
-fuse.run();
+  get mode() {
+    if (this.forceMode) return this.forceMode
+    return process.env.NODE_ENV === "production" ? "prod" : "dev"
+  }
+
+  getConfig() {
+    return FuseBox.init({
+      homeDir: ".",
+      target: "server@es5",
+      output: "dist/$name.js",
+      plugins: this.mode === "prod" ? [QuantumPlugin({ uglify: true, treeshake: true })] : []
+    })
+  }
+}
+
+context(FuseboxConfig)
+
+task("clean", async () => {
+  await src("./out")
+    .clean("dist/")
+    .exec()
+})
+
+task("build", ["clean"], async (ctx: FuseboxConfig) => {
+  ctx.forceMode = "prod"
+  const fuse = ctx.getConfig()
+  fuse.bundle("bin").instructions("> main.ts")
+  fuse.run()
+})
+
+task("watch", ["clean"], async (ctx: FuseboxConfig) => {
+  const fuse = ctx.getConfig()
+  fuse
+    .bundle("bin")
+    .instructions("> [main.ts]")
+    .watch()
+  fuse.run()
+})
+
+task("default", ["build"])
